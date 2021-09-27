97e96435ffa721885ab00c7452041dd52619d195
dmchu <Q3X-o8X-bra-fG2>
2021-09-27 07:51:05 -0400

    refactor parametrized fixtures in conftest.py

--- day_3/conftest.py
+++ day_3/conftest.py
@@ -4,12 +4,19 @@ from pytest import fixture
 data_path = 'tests/test_data.json'
 
 
-def load_test_data(path):
+def load_test_data(path, key):
     with open(path) as data_file:
         data = json.load(data_file)
-        return data
+        return data[key]
 
-@fixture(params=load_test_data(data_path))
+
+@fixture(params=load_test_data(data_path,"names"))
 def names(request):
-    data = request.param
-    return data
+    params = request.param
+    return params
+
+
+@fixture(params=load_test_data(data_path, "exclude_params"))
+def exclude_params(request):
+    params = request.param
+    return params
